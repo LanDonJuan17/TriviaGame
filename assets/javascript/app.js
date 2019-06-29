254ed38112b9950b26be1af1a5f9bce27ce86bd6
Landon Evans <evans.landon@gmail.com>
2019-06-29 08:32:48 -0500

    found images, wrote html, and started writing the game

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var options = [
+        {
+            question: "The Gang consists of Dennis, Mac, Charlie, Dee, &...?",
+            choices: ["Cricket", "Frank", "Dooley", "Schmidty"],
+            answer: 1,
+            picture: "assets/images/frank.jpg"
+        },
+        {
+            question: "Rickety Cricket's real name is?",
+            choices: ["Carl Undergard", "Tim Murphy", "Matthew Mara", "Dylan Toback"],
+            answer: 2,
+            picture: "assets/images/ricketycricket.jpg"
+        },
+        {
+            question: "What was Mac's Dad Luther's former profession?",
+            choices: ["Meth Dealer", "Manager of Jiffy Lube", "Lawyer", "Bartender"],
+            answer: 0,
+            picture: "assets/images/luther.jpg"
+        }
+    ]
